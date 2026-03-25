c3c79ee5ab4da3e3cbe89d64397a212ce3e87137
Facu <gabylope05@gmail.com>
2026-03-25 17:26:34 -0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,6 @@ mod models;
 mod repo;
 mod runtime;
 
-use fzf::*;
-use repo::*;
-
 const DEFAULT_CONFIG_PATH: &str = "~/.config/findgit/config.json";
 
 fn main() {
@@ -15,16 +12,16 @@ fn main() {
 
     let app_config = config::load_config(DEFAULT_CONFIG_PATH);
 
-    let repo_paths = collect_repo_paths(&app_config);
-    let mut repos = build_repos(&repo_paths);
+    let repo_paths = repo::collect_repo_paths(&app_config);
+    let mut repos = repo::build_repos(&repo_paths);
 
-    sort_repos(&mut repos);
+    repo::sort_repos(&mut repos);
 
-    let mut child = build_fzf_command().spawn().expect("Failed to start fzf");
+    let mut child = fzf::build_fzf_command().spawn().expect("Failed to start fzf");
 
     if let Some(mut stdin) = child.stdin.take() {
-        send_repos_to_fzf(&repos, &mut stdin);
+        fzf::send_repos_to_fzf(&repos, &mut stdin);
     }
 
-    handle_fzf_output(child);
+    fzf::handle_fzf_output(child);
 }
